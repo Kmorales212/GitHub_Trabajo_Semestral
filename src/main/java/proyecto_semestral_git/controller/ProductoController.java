df3e5825adffc76b7a5abfdc9d146d206d3a5a53
Kmorales212 <ke.moralese@duocuc.cl>
2025-10-31 16:08:37 -0300

    se crea producto controller y se realizan los endpoints de listar todos los productos y buscar por su id

---
+++ src/main/java/proyecto_semestral_git/controller/ProductoController.java
@@ -0,0 +1,37 @@
+
+package proyecto_semestral_git.controller;
+
+import proyecto_semestral_git.model.ProductoModel;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RestController
+@RequestMapping("/productos")
+public class ProductoController {
+    private final ConcurrentMap<Integer, ProductoModel> repo = new ConcurrentHashMap<>();
+    private final AtomicInteger idGenerator = new AtomicInteger(1);
+
+    @GetMapping
+    public List<ProductoModel> listar() {
+        return new ArrayList<>(repo.values());
+    }
+
+    @GetMapping("/obetener/{id}")
+    public ResponseEntity<ProductoModel> obtener(@PathVariable int id) {
+        ProductoModel p = repo.get(id);
+        if (p == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(p);
+    }
+
+}
